3563694d45e6404b11f17d7206be442bffaa88db
Cris Barbero <cfbarbero@gmail.com>
2014-11-25 13:44:35 -0700

    Fix package name

---
+++ queueexplorer-professional/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'QueueExplorer'
+$installerType = 'EXE' 
+$url = 'http://www.cogin.com/download/mq/download.php?file=QueueExplorerPro3.3.1.exe'
+$silentArgs = '/silent'
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"   -validExitCodes $validExitCodes
+
+
